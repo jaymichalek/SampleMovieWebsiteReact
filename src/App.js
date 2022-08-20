b5001c44245c41660199ede66a91efde655741ab
jaymichalek <jaycoinminer@gmail.com>
2022-08-20 14:54:05 -0700

    Created Front End Using React Followed Tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//HOW TO QUERY AN API:
+const API_URL = 'http://www.omdbapi.com?apikey=YOUROWNKEY'
+
+const movie1 = {
+    "Title": "Superman Returns",
+    "Year": "2006",
+    "imdbID": "tt0348150",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNzY2ZDQ2MTctYzlhOC00MWJhLTgxMmItMDgzNDQwMDdhOWI2XkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg"
 }
 
+//const App -- is like the Main() function in C#!!!
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);   //.Search (The name of the first array) to show only data of movies
+    }
+
+    useEffect(() => {
+        searchMovies('Superman');
+    }, []);
+
+    //IMPORTED App.css for styling and search.svg for search box
+    //className --> this styles it and corresponds with the imports
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0
+                ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} />
+                    ))}
+                </div>
+                ) : 
+                (
+                <div className="empty">
+                    <h2>No movies found</h2>
+                </div>
+                )}
+        </div>
+    );
+}
+
+//Need this code to export components and call it somewhere else!
 export default App;
